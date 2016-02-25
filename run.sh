5e9394ba5102fce233f9a7157578d5a02f9f4353
Keegan Mullaney <keeganmullaney@gmail.com>
2016-02-25 23:18:17 +0800

    add trap for back option, debug mode
    clean up code and simplify functions
    
     Changes to be committed:
    	modified:   run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,12 @@ echo "#                                             "
 echo "# http://keegoid.mit-license.org              "
 echo "# --------------------------------------------"
 
+# set to true prevent clearing the screen
+DEBUG=false
+
+# project directory
+PROJECT_DIRECTORY="$PWD"
+
 # library files
 LIBS='base.sh software.sh git.sh'
 LIBS_DIR='includes'
@@ -26,6 +32,10 @@ done
 
 # config for server
 IS_SERVER=$(confirm "Is this a server?")
+echo "please wait..."
+# make sure curl and git are installed
+install_apt "curl git"
+#install_apt "curl git openssh-server"
 
 # user inputs
 read -ep "enter your name for git: " -i 'Keegan Mullaney' REAL_NAME
@@ -37,152 +47,79 @@ read -ep "enter apps to install with apt-get: " -i 'deluge gist gnupg2 gufw lynx
 read -ep "enter apps to install with pip: " -i 'jrnl[encrypted]' PIP_PROGRAMS
 read -ep "enter apps to install with npm: " -i 'doctoc' NPM_PROGRAMS
 
-# make sure curl and git are installed
-install_apt "curl git"
-#install_apt "curl git openssh-server"
-
-# local repository location
-# use Dropbox for Repos directory?
-#DROPBOX=confirm "Are you using Dropbox for your repositories?"
-#echo
-#REPOS=$(locate_repos $USER_NAME $DROPBOX)
-#echo "repository location: $REPOS"
-
-# install software and update system
-function updates_go()
-{
-   echo "# -------------------------------"
-   echo "# SECTION 1: INSTALLS & UPDATES  "
-   echo "# -------------------------------"
-
-   # install and update software
-   run_script linux_update.sh
-   pause
-}
- 
-# git
-function git_go()
-{
-   echo "# -------------------------------"
-   echo "# SECTION 2: GIT CONFIG          "
-   echo "# -------------------------------"
-
-   # setup git
-   run_script git_config.sh
-   pause
-}
-
-# ssh
-function ssh_go()
-{
-   echo "# -------------------------------"
-   echo "# SECTION 3: SSH KEY             "
-   echo "# -------------------------------"
-
-   # setup git
-   run_script ssh_key.sh
-   pause
-}
-
-# aliases
-function aliases_go()
-{
-   echo "# -------------------------------"
-   echo "# SECTION 4: ALIASES             "
-   echo "# -------------------------------"
-
-   # add useful aliases
-   run_script aliases.sh
-   pause
-}
-
-# config
-function terminal_go()
-{
-   echo "# -------------------------------"
-   echo "# SECTION 5: TERMINAL CONFIG     "
-   echo "# -------------------------------"
-
-   # setup the terminal
-   run_script terminal_config.sh
-   pause
-}
-
-# wordpress
-function wordpress_go()
-{
-   echo "# -------------------------------"
-   echo "# SECTION 6: WORDPRESS DEV       "
-   echo "# -------------------------------"
-
-   # install requirements for WordPress development
-   run_script wordpress_dev.sh
-   pause
-}
-
 # code to run before exit
 function finish_up()
 {
    # set ownership
+   pause "Press [Enter] to make sure all files in $HOME are owned by $(logname)" true
    sudo chown -cR $(logname):$(logname) "$HOME"
    echo
-   echo "# --------------------------------------------------------------------"
-   echo "# Lastly: execute sudo ./sudoers.sh to increase the sudo timeout.     "
-   echo "# --------------------------------------------------------------------"
+   echo -e "${LIGHT_GRAY} Lastly: execute sudo ./sudoers.sh to increase the sudo timeout. ${STD}"
    echo
    echo "Thanks for using this ubuntu-quick-config script."
-   echo
 }
 
+# --------------------------------------------
 # display the menu
-display_menu()
+# --------------------------------------------
+function display_menu()
 {
-   clear
-   echo "~~~~~~~~~~~~~~~~~~~~~~~"	
+      [ "$DEBUG" = true ] || clear
+      echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
    if [ "$IS_SERVER" = true ]; then
-      echo "   M A I N - M E N U   "
-      echo "        server         "
+      echo "     M A I N - M E N U     "
+      echo "          server           "
    else
-      echo "   M A I N - M E N U   "
-      echo "      workstation      "
+      echo "     M A I N - M E N U     "
+      echo "        workstation        "
    fi
-   echo "~~~~~~~~~~~~~~~~~~~~~~~"
-   echo "1. INSTALLS & UPDATES"
-   echo "2. GIT CONFIG"
-   echo "3. SSH KEY"
-   echo "4. ALIASES"
-   echo "5. TERMINAL CONFIG"
-   echo "6. WORDPRESS DEVELOPMENT"
-   echo "7. EXIT"
+      echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+      echo "1. INSTALLS & UPDATES"
+      echo "2. GIT CONFIG"
+      echo "3. SSH KEY"
+      echo "4. ALIASES"
+      echo "5. TERMINAL CONFIG"
+      echo "6. WORDPRESS DEVELOPMENT"
+      echo "7. EXIT"
 }
 
+# --------------------------------------------
 # user selection
-select_options()
+# --------------------------------------------
+function select_options()
 {
    local choice
+   # make sure we're always starting from the right place
+   cd "$PROJECT_DIRECTORY"
    read -rp "Enter choice [1 - 7]: " choice
    case $choice in
-      1) updates_go;;
-      2) git_go;;
-      3) ssh_go;;
-      4) aliases_go;;
-      5) terminal_go;;
-      6) wordpress_go;;
+      1) run_script linux_update.sh    $DEBUG;;
+      2) run_script git_config.sh      $DEBUG;;
+      3) run_script ssh_key.sh         $DEBUG;;
+      4) run_script aliases.sh         $DEBUG;;
+      5) run_script terminal_config.sh $DEBUG;;
+      6) run_script wordpress_dev.sh   $DEBUG;;
       7) finish_up && exit 0;;
       *) echo -e "${RED} Error... ${STD}" && sleep 1
    esac
 }
-"\033[0;41;30mError...\033[0;0;39m"
-# ----------------------------------------------
-# trap CTRL+C, CTRL+Z and quit singles
-# ----------------------------------------------
-#trap '' SIGINT SIGQUIT SIGTSTP
- 
-# -----------------------------------
+
+# --------------------------------------------
+# trap Ctrl+Z to return to the main menu
+# --------------------------------------------
+trap "echo; menu_loop" SIGTSTP
+
+# --------------------------------------------
 # main loop (infinite)
-# ------------------------------------
-while true; do
-   display_menu
-   select_options
-done
+# --------------------------------------------
+function menu_loop()
+{
+   while true; do
+      display_menu
+      select_options
+      pause
+   done
+}
+# call menu loop before program end
+menu_loop
 
