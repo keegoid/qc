70cfb27d6697cf187f7adf477fc2a477a8562797
Keegan Mullaney <keeganmullaney@gmail.com>
2016-03-02 02:17:35 +0800

    make backup and swp directories
    vim can store backup and swap files in ~/.vim
    
     Changes to be committed:
    	modified:   scripts/terminal_config.sh

--- scripts/terminal_config.sh
+++ scripts/terminal_config.sh
@@ -75,7 +75,7 @@ fi
 
 # install vim plugins and colorthemes
 [ -d "$HOME/.vim/autoload/pathogen" ] || git clone https://github.com/tpope/vim-pathogen.git $HOME/.vim/autoload/pathogen && cp -n $HOME/.vim/autoload/pathogen/autoload/pathogen.vim $HOME/.vim/autoload && echo "vim plugin pathogen was installed"
-[ -f "$HOME/.vim/colors/blackboard.vim" ] || mkdir "$HOME/.vim/colors" && cp -n "$PROJECT/includes/blackboard.vim" "$HOME/.vim/colors/" && echo "vim colortheme blackboard was installed"
+[ -f "$HOME/.vim/colors/blackboard.vim" ] || mkdir -p "$HOME/.vim/colors" && cp -n "$PROJECT/includes/blackboard.vim" "$HOME/.vim/colors/" && echo "vim colortheme blackboard was installed"
 [ -d "$HOME/.vim/bundle/gundo" ] || git clone https://github.com/sjl/gundo.vim.git $HOME/.vim/bundle/gundo && echo "vim plugin gundo was installed"
 [ -d "$HOME/.vim/bundle/ag" ] || git clone https://github.com/rking/ag.vim.git $HOME/.vim/bundle/ag && echo "vim plugin ag was installed"
 [ -d "$HOME/.vim/bundle/ctrlp" ] || git clone https://github.com/ctrlpvim/ctrlp.vim.git $HOME/.vim/bundle/ctrlp && echo "vim plugin ctrlp was installed"
@@ -86,6 +86,8 @@ if grep -q ":so ~/$CONFIG/.vim_config" $HOME/.vimrc >/dev/null 2>&1; then
 else
    pause "Press [Enter] to configure .vimrc" true
    cp -n "$PROJECT/includes/.vim_config" "$HOME/$CONFIG"
+   mkdir -p "$HOME/.vim/backup"
+   mkdir -p "$HOME/.vim/swp"
 cat << EOF >> "$HOME/.vimrc"
 " source config file
 :so ~/$CONFIG/.vim_config
