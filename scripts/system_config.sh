66999516ba2e711bbcd448c3246750f6ba38533b
Keegan Mullaney <keeganmullaney@gmail.com>
2016-03-05 22:34:52 +0800

    organize code into functions and main
    merge git script in
    
     Changes to be committed:
    	modified:   scripts/system_config.sh

--- scripts/system_config.sh
+++ scripts/system_config.sh
@@ -9,154 +9,231 @@ echo "#                                             "
 echo "# http://keegoid.mit-license.org              "
 echo "# --------------------------------------------"
 
+# --------------------------  SETUP PARAMETERS
+
 read -ep "Directory to use for config files: ~/" -i "Dropbox/Config" CONFIG
 
-# --------------------------------------------
-# .bashrc
-# --------------------------------------------
+# --------------------------  ALIASES
 
-# aliases
-REPO_DIR="$HOME/$CONFIG/bash"
-REPO_URL=https://gist.github.com/9d74e08779c1db6cb7b7
-CONF_PATH=$HOME/.bashrc
-REPO_PATH=$REPO_DIR/bash_aliases
-REPO_FILE=$(trim_longest_left_pattern $REPO_PATH "/")
-SRC_CMD="\n# source $REPO_FILE\n
-if [ -f $REPO_PATH ]; then\n
-    . $REPO_PATH\n
-fi"
+set_aliases() {
+   local conf_file_path="$1"
+   local repo_url="$2"
+   local repo_dir=$(trim_shortest_right_pattern "$3" "/")
+   local repo_file_path="$3"
+   local src_cmd="$4"
 
-# if config file exists, update config, otherwise clone it
-if [ -f $REPO_PATH ]; then
-   cd $REPO_DIR && echo "updating $REPO_FILE..." && git pull && echo "successfully updated: $REPO_PATH" && cd - >/dev/null
-else
-   pause "Press [Enter] to configure $CONF_PATH" true
-   git clone $REPO_URL $REPO_DIR && echo -e "$SRC_CMD" >> $CONF_PATH && source $CONF_PATH && echo "successfully configured: $CONF_PATH"
-fi
+   if [ -f $repo_file_path ]; then
+      pause "Press [Enter] to update $repo_file_path" true
+      cd $repo_dir && echo "updating $repo_file_path..." && git pull && cd - >/dev/null
+   else
+      pause "Press [Enter] to configure $conf_file_path" true
+      git clone $repo_url $repo_dir && echo -e "$src_cmd" >> $conf_file_path && source $conf_file_path && success "successfully configured: $conf_file_path"
+   fi
+}
 
-# autojump
-SRC_CMD="\n# source autojump.sh\n
-. /usr/share/autojump/autojump.sh"
+# --------------------------  AUTOJUMP (so awesome)
 
-if grep -q "autojump/autojump.sh" $CONF_PATH >/dev/null 2>&1; then
-   echo "already added autojump (usage: j directory)"
-else
-   pause "Press [Enter] to configure autojump for the gnome-terminal" true
-   echo -e "$SRC_CMD" >> $CONF_PATH && source $CONF_PATH && echo "successfully configured: $CONF_PATH"
-fi
+set_autojump() {
+   local conf_file_path="$1"
+   local src_cmd="$2"
 
-# color terminal prompts
-if grep -q "#force_color_prompt=yes" $CONF_PATH >/dev/null 2>&1; then
-   pause "Press [Enter] to activate color terminal prompts" true
-   sed -i.bak -e "s|#force_color_prompt=yes|force_color_prompt=yes|" $CONF_PATH && source $CONF_PATH && echo "successfully configured: $CONF_PATH with color terminal prompts"
-else
-   echo "already set color prompts"
-fi
+   if grep -q "autojump/autojump.sh" $conf_file_path >/dev/null 2>&1; then
+      notify "already added autojump (usage: j directory)"
+   else
+      pause "Press [Enter] to configure autojump for gnome-terminal" true
+      echo -e "$src_cmd" >> $conf_file_path && source $conf_file_path && success "successfully configured: $conf_file_path"
+   fi
+}
 
-# --------------------------------------------
-# .inputrc
-# --------------------------------------------
+# --------------------------  TERMINAL COLOR PROMPTS
 
+set_terminal_color() {
+   local conf_file_path="$1"
+
+   if grep -q "#force_color_prompt=yes" $conf_file_path >/dev/null 2>&1; then
+      pause "Press [Enter] to activate color terminal prompts" true
+      sed -i.bak -e "s|#force_color_prompt=yes|force_color_prompt=yes|" $conf_file_path && source $conf_file_path && success "successfully configured: $conf_file_path with color terminal prompts"
+   else
+      notify "already set color prompts"
+   fi
+}
+
+# --------------------------  TERMINAL HISTORY LOOKUP (also awesome)
+
+set_terminal_history() {
+   local conf_file_path="$1"
+   local conf_cmd="$2"
+
+   if grep -q "backward-char" $conf_file_path >/dev/null 2>&1; then
+      notify "already added terminal history lookup"
+   else
+      pause "Press [Enter] to configure .inputrc" true
+cat << 'EOF' > $conf_file_path 
 # terminal history lookup
-CONF_PATH=$HOME/.inputrc
-SRC_CMD="# terminal history lookup\n
-'\e[A': history-search-backward\n
-'\e[B': history-search-forward\n
-'\e[C': forward-char\n
-'\e[D': backward-char"
+'\e[A': history-search-backward
+'\e[B': history-search-forward
+'\e[C': forward-char
+'\e[D': backward-char
+EOF
+   fi
 
-if grep -q $CONFIG/.input_config $CONF_PATH >/dev/null 2>&1; then
-   echo "already added terminal history lookup"
-else
-   pause "Press [Enter] to configure .inputrc" true
-   echo -e "$SRC_CMD" > $CONF_PATH && source $CONF_PATH && echo "successfully configured: $CONF_PATH"
-fi
+   REF=$?
+   success "successfully configured: $conf_file_path"
+}
 
-# --------------------------------------------
-# .muttrc solarized colorscheme
-# --------------------------------------------
+# --------------------------  TERMINAL PROFILE
 
-REPO_DIR=$HOME/$CONFIG/mutt/mutt-colors-solarized
-REPO_URL=https://github.com/altercation/mutt-colors-solarized.git
-REPO_PATH=$REPO_DIR/mutt-colors-solarized-dark-16.muttrc
-CONF_PATH=$HOME/$CONFIG/mutt/mutt.conf
-COLOR_FILE=$(trim_longest_left_pattern $REPO_DIR "/")
+set_terminal_profile() {
+   local repo_url="$1"
+   local repo_dir=$(trim_shortest_right_pattern "$2" "/")
+   local repo_file_path="$2"
 
-# if config file exists, update config, otherwise clone it
-if [ -d $REPO_DIR ]; then
-   cd $REPO_DIR && echo "updating $COLOR_FILE..." && git pull && echo "successfully updated: $COLOR_FILE" && cd - >/dev/null
-else
-   pause "Press [Enter] to configure $COLOR_FILE" true
-   git clone $REPO_URL $REPO_DIR && echo "successfully configured: $COLOR_FILE"
-fi
+   # solarized color scheme
+   if [ -f $repo_file_path ]; then
+      pause "Press [Enter] to update $repo_file_path" true
+      cd $repo_dir && echo "updating $repo_file_path..." && git pull && { run_script install.sh; RET=$?; success "successfully updated: gnome-terminal profile"; } && cd - >/dev/null
+   else
+      pause "Press [Enter] to run $repo_file_path" true
+      git clone $repo_url $repo_dir && run_script install.sh && success "successfully configured: gnome-terminal with solarized colors"
+   fi
+}
 
-# --------------------------------------------
-# terminal solarized profile
-# --------------------------------------------
+# --------------------------  MUTT
 
-REPO_DIR=$HOME/$CONFIG/terminal
-REPO_URL=https://github.com/Anthony25/gnome-terminal-colors-solarized.git
-REPO_PATH=$REPO_DIR/install.sh
+set_mutt_colors() {
+   local conf_file_path="$1"
+   local repo_url="$2"
+   local repo_dir=$(trim_shortest_right_pattern "$3" "/")
+   local repo_file_path="$3"
+   local src_cmd="$4"
 
-# if config directory exists, update config, otherwise clone it
-if [ -d $REPO_DIR ]; then
-   cd $REPO_DIR && echo "updating gnome-terminal solarized profile..." && git pull && run_script install.sh && echo "successfully updated: gnome-terminal with solarized colors" && cd - >/dev/null
-else
-   pause "Press [Enter] to configure gnome-terminal solarized profile" true
-   git clone $REPO_URL $REPO_DIR && run_script install.sh && echo "successfully configured: gnome-terminal with solarized colors"
-fi
+   # solarized color scheme
+   if [ -f $repo_file_path ]; then
+      pause "Press [Enter] to update $repo_file_path" true
+      cd $repo_dir && echo "updating $repo_file_path..." && git pull && cd - >/dev/null
+   else
+      pause "Press [Enter] to configure $conf_file_path" true
+      git clone $repo_url $repo_dir && echo -e "$src_cmd" >> $conf_file_path && success "successfully configured: $conf_file_path"
+   fi
+}
 
-# --------------------------------------------
-# .tmux.conf
-# --------------------------------------------
+# --------------------------  TMUX
 
-REPO_DIR=$HOME/$CONFIG/tmux
-REPO_URL=https://gist.github.com/3247d5a1c172167e593c.git
-CONF_PATH=$HOME/.tmux.conf
-REPO_PATH=$REPO_DIR/tmux.conf
-CONF_FILE=$(trim_longest_left_pattern $CONF_PATH "/")
+set_tmux_config() {
+   local conf_file_path="$1"
+   local repo_url="$2"
+   local repo_dir=$(trim_shortest_right_pattern "$3" "/")
+   local repo_file_path="$3"
+   local src_cmd="$4"
 
-# if config directory exists, update config, otherwise clone it
-if [ -d $REPO_DIR ]; then
-   cd $REPO_DIR && echo "updating $CONF_FILE..." && git pull && echo "successfully updated: $CONF_PATH" && cd - >/dev/null
-else
-   pause "Press [Enter] to configure $CONF_FILE" true
-   git clone $REPO_URL $REPO_DIR && echo "source-file $REPO_PATH" > $CONF_PATH && echo "successfully configured: $CONF_PATH"
-fi
+   if [ -f $repo_file_path ]; then
+      pause "Press [Enter] to update $repo_file_path" true
+      cd $repo_dir && echo "updating $repo_file_path..." && git pull && cd - >/dev/null
+   else
+      pause "Press [Enter] to configure $conf_file_path" true
+      git clone $repo_url $repo_dir && echo "$src_cmd" > $conf_file_path && success "successfully configured: $conf_file_path"
+   fi
+}
 
-# --------------------------------------------
-# gedit
-# --------------------------------------------
+# --------------------------  GEDIT
 
-REPO_DIR=$HOME/$CONFIG/gedit/blackboard
-REPO_URL=https://github.com/afair/dot-gedit.git
-CONF_PATH=$HOME/.local/share/gedit/styles/blackboard.xml
-REPO_PATH=$REPO_DIR/blackboard.xml
-CONF_FILE=$(trim_longest_left_pattern $CONF_PATH "/")
+set_gedit_colors() {
+   local conf_file_path="$1"
+   local repo_url="$2"
+   local repo_dir=$(trim_shortest_right_pattern "$3" "/")
+   local repo_file_path="$3"
 
-# if config directory exists, update config, otherwise clone it
-if [ -d $REPO_DIR ]; then
-   cd $REPO_DIR && echo "updating $CONF_FILE..." && git pull && cp $REPO_PATH $CONF_PATH && echo "successfully updated: $CONF_PATH" && cd - >/dev/null
-else
-   pause "Press [Enter] to configure $CONF_FILE" true
-   git clone $REPO_URL $REPO_DIR && cp $REPO_PATH $CONF_PATH && echo "successfully configured: $CONF_PATH"
-fi
+   if [ -f $repo_file_path ]; then
+      pause "Press [Enter] to update $repo_file_path" true
+      cd $repo_dir && echo "updating $repo_file_path..." && git pull && cp $repo_file_path $conf_file_path && cd - >/dev/null
+   else
+      pause "Press [Enter] to configure $conf_file_path" true
+      git clone $repo_url $repo_dir && cp $repo_file_path $conf_file_path && success "successfully configured: $conf_file_path"
+   fi
+}
 
-# --------------------------------------------
-# .vimrc.local for use with spf13-vim
-# --------------------------------------------
+# --------------------------  VIM
 
-REPO_DIR=$HOME/$CONFIG/vim
-REPO_URL=https://gist.github.com/00a60c7355c27c692262
-CONF_PATH=$HOME/.vimrc.local
-REPO_PATH=$REPO_DIR/vim.conf
-CONF_FILE=$(trim_longest_left_pattern $CONF_PATH "/")
+set_vim_config() {
+   local conf_file_path="$1"
+   local repo_url="$2"
+   local repo_dir=$(trim_shortest_right_pattern "$3" "/")
+   local repo_file_path="$3"
+   local src_cmd="$4"
 
-# check if vim is already configured
-if [ -d $REPO_DIR ]; then
-   cd $REPO_DIR && echo "updating $CONF_FILE..." && git pull && echo "successfully updated: $CONF_PATH" && cd - >/dev/null
-else
-   pause "Press [Enter] to configure $CONF_FILE" true
-   git clone $REPO_URL $REPO_DIR && echo ":so $REPO_PATH" > $CONF_PATH && echo "successfully configured: $CONF_PATH"
-fi
+   if [ -f $repo_file_path ]; then
+      pause "Press [Enter] to update $repo_file_path" true
+      cd $repo_dir && echo "updating $repo_file_path..." && git pull && cd - >/dev/null
+   else
+      pause "Press [Enter] to configure $conf_file_path" true
+      git clone $repo_url $repo_dir && echo -e "$src_cmd" > $conf_file_path && success "successfully configured: $conf_file_path"
+   fi
+}
 
+# --------------------------  GIT
+
+set_git_ignore() {
+   local conf_file_path="$1"
+   local repo_url="$2"
+   local repo_dir=$(trim_shortest_right_pattern "$3" "/")
+   local repo_file_path="$3"
+
+   # global ignore
+   if [ -f $repo_file_path ]; then
+      pause "Press [Enter] to update $repo_file_path" true
+      cd $repo_dir && echo "updating $repo_file_path..." && git pull && cp $repo_file_path $conf_file_path && cd - >/dev/null
+   else
+      pause "Press [Enter] to configure $conf_file_path" true
+      git clone $repo_url $repo_dir && cp $repo_file_path $conf_file_path
+      read -ep "your name for git commit logs: " -i 'Keegan Mullaney' real_name
+      read -ep "your email for git commit logs: " -i 'keeganmullaney@gmail.com' email_address
+      read -ep "your preferred text editor for git commits: " -i 'vi' git_editor
+      configure_git "$real_name" "$email_address" "$git_editor"
+      RET=$?
+      success "successfully configured: $conf_file_path"
+   fi
+}
+
+# --------------------------  MAIN
+
+set_aliases          "$HOME/.bashrc" \
+                     "https://gist.github.com/9d74e08779c1db6cb7b7" \
+                     "$HOME/$CONFIG/bash/aliases/bash_aliases" \
+                     "\n# source alias file\nif [ -f ~/$CONFIG/bash/aliases/bash_aliases ]; then\n   . ~/$CONFIG/bash/aliases/bash_aliases\nfi"
+
+set_autojump         "$HOME/.bashrc" \
+                     "\n# source autojump file\nif [ -f /usr/share/autojump/autojump.sh ]; then\n   . /usr/share/autojump/autojump.sh\nfi"
+
+set_terminal_color   "$HOME/.bashrc"
+
+set_terminal_history "$HOME/.inputrc"
+
+set_terminal_profile "https://github.com/Anthony25/gnome-terminal-colors-solarized.git" \
+                     "$HOME/$CONFIG/terminal/profile/install.sh"
+
+set_mutt_colors      "$HOME/.muttrc" \
+                     "https://github.com/altercation/mutt-colors-solarized.git" \
+                     "$HOME/$CONFIG/mutt/colors/mutt-colors-solarized-dark-16.muttrc" \
+                     "# source colorscheme file\nsource ~/$CONFIG/mutt/colors/mutt-colors-solarized-dark-16.muttrc"
+
+set_tmux_config      "$HOME/.tmux.conf" \
+                     "https://gist.github.com/3247d5a1c172167e593c.git" \
+                     "$HOME/$CONFIG/tmux/tmux.conf" \
+                     "source-file ~/$CONFIG/tmux/tmux.conf"
+
+set_gedit_colors     "$HOME/.local/share/gedit/styles/blackboard.xml" \
+                     "https://github.com/afair/dot-gedit.git" \
+                     "$HOME/$CONFIG/gedit/blackboard/blackboard.xml"
+
+set_gedit_colors     "$HOME/.local/share/gedit/styles/solarized-dark.xml" \
+                     "https://github.com/mattcan/solarized-gedit.git" \
+                     "$HOME/$CONFIG/gedit/solarized/solarized-dark.xml"
+
+set_vim_config       "$HOME/.vimrc.local" \
+                     "https://gist.github.com/00a60c7355c27c692262.git" \
+                     "$HOME/$CONFIG/vim/vim.conf" \
+                     "\" source config file\n:so ~/$CONFIG/vim/vim.conf"
+
+set_git_ignore       "$HOME/.gitignore_global" \
+                     "https://gist.github.com/efa547b362910ac7077c.git" \
+                     "$HOME/$CONFIG/git/gitignore_global"
