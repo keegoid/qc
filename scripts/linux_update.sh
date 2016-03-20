2798f9044b604fc624642134bfb22b2a022bc21c
Keegan Mullaney <keeganmullaney@gmail.com>
2016-03-20 22:23:07 +0800

    fix bugs with ruby and nodejs lts installs
    also fix npm list so it only returns exact match for jekyll using
    jekyll$
    
     Changes to be committed:
    	modified:   scripts/linux_update.sh

--- scripts/linux_update.sh
+++ scripts/linux_update.sh
@@ -12,6 +12,7 @@ echo "# --------------------------------------------"
 # --------------------------  SETUP PARAMETERS
 
 [ -z "$RUBY_V" ] && RUBY_V='2.2.3'
+# https://www.ruby-lang.org/en/
 
 # --------------------------  MISSING PROGRAM CHECKS
 
@@ -32,7 +33,7 @@ pip_check_list=()
 # install ruby with rbenv and ruby-build
 install_rbenv_ruby() {
     # rbenv
-    set_sourced_config  "$HOME/.profile" \
+    set_sourced_config  "$HOME/.bashrc" \
                         "https://github.com/rbenv/rbenv.git" \
                         "$HOME/.rbenv/" \
                         '[[ ":$PATH:" =~ ":$HOME/.rbenv/bin:" ]] || PATH="$HOME/.rbenv/bin:$PATH"'
@@ -41,22 +42,22 @@ install_rbenv_ruby() {
     [ -d "$HOME/.rbenv" ] && cd "$HOME/.rbenv" && src/configure && make -C src && cd - >/dev/null
 
     # add rbenv init - command to .profile
-    set_source_cmd      "$HOME/.profile" \
-                        '"eval "$(rbenv init -)"' \
+    set_source_cmd      "$HOME/.bashrc" \
+                        'rbenv/shims:' \
                         '[[ ":$PATH:" =~ ":$HOME/.rbenv/shims:" ]] || eval "$(rbenv init -)"'
 
     # ruby-build
-    set_sourced_config  "$HOME/.profile" \
+    set_sourced_config  "$HOME/.bashrc" \
                         "https://github.com/rbenv/ruby-build.git" \
                         "$HOME/.rbenv/plugins/ruby-build/" \
                         '[[ ":$PATH:" =~ ":$HOME/.rbenv/plugins/ruby-build/bin:" ]] || PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"'
 
     # tell rubygems not to install docs for each package locally
     set_source_cmd      "$HOME/.gemrc" \
-                        'gem: --no-ri --no-rdoc' \
+                        'no-rdoc' \
                         'gem: --no-ri --no-rdoc'
 
-    source "$HOME/.profile"
+    source "$HOME/.bashrc"
 
     # check that rbenv is working
     type rbenv
@@ -76,8 +77,9 @@ install_rbenv_ruby() {
 
 # install the long terms support version of Node.js
 install_npm_lts() {
-    curl https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-    curl -L https://deb.nodesource.com/setup_4.x | sudo -E bash -
+    curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+    echo "deb https://deb.nodesource.com/node_4.x trusty main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update
     sudo apt-get -y install nodejs
 
     # check node version
@@ -89,29 +91,6 @@ install_npm_lts() {
 
 # --------------------------  CHECK FOR MISSING PROGRAMS
 
-# loop through check list and add missing packages to install list
-apt_check() {
-    local pkg
-    local pkg_version
-
-    for pkg in "${apt_check_list[@]}"; do
-        if not_installed $pkg; then
-            echo -e " ${YELLOW_BLACK} * $pkg [not installed] ${NONE_WHITE}"
-            apt_install_list+=($pkg)
-        else
-            pkg_version=$(dpkg -s "${pkg}" 2>&1 | grep 'Version:' | cut -d " " -f 2)
-            space_count="$(expr 20 - "${#pkg}")"
-            pack_space_count="$(expr 20 - "${#pkg_version}")"
-            real_space="$(expr ${space_count} + ${pack_space_count} + ${#pkg_version})"
-            echo -en " ${GREEN_CHK}"
-            printf " $pkg %${real_space}.${#pkg_version}s ${pkg_version}\n"
-        fi
-    done
-
-    RET="$?"
-    debug
-}
-
 # loop through check list and add missing gems to install list
 gem_check() {
     local pkg
@@ -119,7 +98,7 @@ gem_check() {
 
     for pkg in "${gem_check_list[@]}"; do
         if gem list $pkg -i >/dev/null; then
-            pkg_version=$(gem list "${pkg}" | grep "${pkg}" | cut -d " " -f 2 | cut -d "(" -f 2 | cut -d ")" -f 1)
+            pkg_version=$(gem list $pkg$ | grep "$pkg" | cut -d " " -f 2 | cut -d "(" -f 2 | cut -d ")" -f 1)
             space_count="$(expr 20 - "${#pkg}")"
             pack_space_count="$(expr 20 - "${#pkg_version}")"
             real_space="$(expr ${space_count} + ${pack_space_count} + ${#pkg_version})"
@@ -183,7 +162,101 @@ pip_check() {
     debug
 }
 
+# loop through check list and add missing packages to install list
+apt_check() {
+    local pkg
+    local pkg_version
+
+    for pkg in "${apt_check_list[@]}"; do
+        if not_installed $pkg; then
+            echo -e " ${YELLOW_BLACK} * $pkg [not installed] ${NONE_WHITE}"
+            apt_install_list+=($pkg)
+        else
+            pkg_version=$(dpkg -s "${pkg}" 2>&1 | grep 'Version:' | cut -d " " -f 2)
+            space_count="$(expr 20 - "${#pkg}")"
+            pack_space_count="$(expr 20 - "${#pkg_version}")"
+            real_space="$(expr ${space_count} + ${pack_space_count} + ${#pkg_version})"
+            echo -en " ${GREEN_CHK}"
+            printf " $pkg %${real_space}.${#pkg_version}s ${pkg_version}\n"
+        fi
+    done
+
+    RET="$?"
+    debug
+}
+
 # --------------------------  INSTALL MISSING PROGRAMS
+
+# loop through install list and install any gems that are in the list
+gem_install() {
+    program_must_exist 'libssl-dev'
+    program_must_exist 'libreadline-dev'
+    program_must_exist 'zlib1g-dev'
+
+    # make sure ruby is installed
+    confirm "Install the latest version of ruby with rbenv and ruby-build?" true
+    [ "$?" -eq 0 ] && install_rbenv_ruby || { ruby -v | grep "not installed" && program_must_exist ruby; }
+#    program_must_exist "rubygems-integration"
+
+    gem_check
+
+    if [[ "${#gem_install_list[@]}" -eq 0 ]]; then
+        notify "No gems to install"
+    else
+        # install required gems
+        pause "Press [Enter] to install gems" true
+        gem install ${gem_install_list[@]}
+    fi
+
+    RET="$?"
+    debug
+}
+
+# loop through install list and install any npms that are in the list
+npm_install() {
+    # make sure npm is installed
+    confirm "Install the long term support version of Node.js?" true
+    [ "$?" -eq 0 ] && install_npm_lts || { node -v | grep "not installed" && program_must_exist npm; }
+
+    npm_check
+
+    # symlink nodejs to path
+    if [ ! -L /usr/bin/node ]; then
+        sudo ln -s "$(which nodejs)" /usr/bin/node
+    fi
+
+    if [[ "${#npm_install_list[@]}" -eq 0 ]]; then
+        notify "No npms to install"
+    else
+        # install required npms
+        pause "Press [Enter] to install npms" true
+        sudo npm install -g ${npm_install_list[@]}
+    fi
+
+    RET="$?"
+    debug
+}
+
+# loop through install list and install any pips that are in the list
+pip_install() {
+    # make sure dependencies are installed
+    program_must_exist "python-pip"
+    program_must_exist "python-keyring"
+    program_must_exist "python-setuptools"
+
+    pip_check
+
+    if [[ "${#pip_install_list[@]}" -eq 0 ]]; then
+        notify "No pips to install"
+    else
+        # install required pips
+        pause "Press [Enter] to install pips" true
+        sudo -H pip install ${pip_install_list[@]}
+    fi
+
+    RET="$?"
+    debug
+}
 
 # loop through install list and install any packages that are in the list
 # $1 -> to update sources or not
@@ -211,72 +284,6 @@ apt_install() {
     debug
 }
 
-# loop through install list and install any gems that are in the list
-gem_install() {
-    gem_check
-
-    # make sure ruby is installed
-    confirm "Install the latest version of ruby with rbenv and ruby-build?" true
-    [ "$?" -eq 0 ] && install_rbenv_ruby || program_must_exist ruby
-    program_must_exist "rubygems-integration"
-
-    if [[ "${#gem_install_list[@]}" -eq 0 ]]; then
-        notify "No gems to install"
-    else
-        # install required gems
-        pause "Press [Enter] to install gems" true
-        gem install ${gem_install_list[@]}
-    fi
-
-    RET="$?"
-    debug
-}
-
-# loop through install list and install any npms that are in the list
-npm_install() {
-    npm_check
-
-    # make sure npm is installed
-    confirm "Install the long term support version of Node.js?" true
-    [ "$?" -eq 0 ] && install_npm_lts || program_must_exist npm
-
-    # symlink nodejs to path
-    if [ ! -L /usr/bin/node ]; then
-        sudo ln -s "$(which nodejs)" /usr/bin/node
-    fi
-
-    if [[ "${#npm_install_list[@]}" -eq 0 ]]; then
-        notify "No npms to install"
-    else
-        # install required npms
-        pause "Press [Enter] to install npms" true
-        sudo npm install -g ${npm_install_list[@]}
-    fi
-
-    RET="$?"
-    debug
-}
-
-# loop through install list and install any pips that are in the list
-pip_install() {
-    pip_check
-
-    # make sure dependencies are installed
-    program_must_exist "python-pip"
-    program_must_exist "python-keyring"
-
-    if [[ "${#pip_install_list[@]}" -eq 0 ]]; then
-        notify "No pips to install"
-    else
-        # install required pips
-        pause "Press [Enter] to install pips" true
-        sudo -H pip install ${pip_install_list[@]}
-    fi
-
-    RET="$?"
-    debug
-}
-
 # --------------------------  64-BIT ARCHITECTURE
 
 UPDATE=0
@@ -289,9 +296,8 @@ fi
 # --------------------------  DEFAULT APT PACKAGES
 
 DEFAULT_SERVER_LIST='ca-certificates gettext-base less man-db openssh-server python-software-properties software-properites-common vim-gtk wget'
-DEFAULT_WORKSTATION_LIST='autojump deluge gnupg2 lynx pinta silversearcher-ag tmux x11vnc xclip vim-gtk vlc'
+DEFAULT_WORKSTATION_LIST='autojump lynx mutt pinta silversearcher-ag tmux x11vnc xclip vim-gtk vlc'
 DEFAULT_DEV_LIST='autoconf automake build-essential checkinstall dconf-cli'
-RUBY_DEPENDENCIES_LIST='libffi-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libxslt1-dev libyaml-dev python-software-properties zlib1g-dev'
 
 # --------------------------  PROMPT FOR PROGRAMS
 
@@ -301,34 +307,30 @@ if [ "$IS_SERVER" -eq 0 ]; then
 else
     notify3 "The following default packages can be modified prior to installation."
     echo
+    echo "GEMs, NPMs, PIPs"
     echo "WORKSTATION"
     echo "DEVELOPER"
-    echo "RUBY DEPENDENCIES"
-    echo "GEMs, NPMs, PIPs"
     echo
-    notify "Workstation packages to install (delete all to skip)"
-    read -ep "   : " -i "$DEFAULT_WORKSTATION_LIST" APTS1
-    notify "Developer packages to install"
-    read -ep "   : " -i "$DEFAULT_DEV_LIST"         APTS2
-    notify "Ruby dependencies to install"
-    read -ep "   : " -i "$RUBY_DEPENDENCIES_LIST"   APTS3
     notify "Packages to install with gem"
     read -ep "   : " -i 'bundler gist jekyll'       GEMS
     notify "Packages to install with npm"
     read -ep "   : " -i 'doctoc'                    NPMS
     notify "Packages to install with pip"
     read -ep "   : " -i 'jrnl[encrypted]'           PIPS
+    notify "Workstation packages to install (delete all to skip)"
+    read -ep "   : " -i "$DEFAULT_WORKSTATION_LIST" APTS1
+    notify "Developer packages to install"
+    read -ep "   : " -i "$DEFAULT_DEV_LIST"         APTS2
 fi
 
 # --------------------------  ARRAY ASSIGNMENTS
 
 # add packages, gems, npms and pips to check list arrays
-apt_check_list+=($APTS1)
-apt_check_list+=($APTS2)
-apt_check_list+=($APTS3)
 gem_check_list+=($GEMS)
 npm_check_list+=($NPMS)
 pip_check_list+=($PIPS)
+apt_check_list+=($APTS1)
+apt_check_list+=($APTS2)
 
 # --------------------------  INSTALL PROGRAMS
 
