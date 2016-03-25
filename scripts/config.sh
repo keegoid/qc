9ebad1ea4ad2db785493ccf26eeff5e75590499c
Keegan Mullaney <keeganmullaney@gmail.com>
2016-03-25 12:45:56 +0800

    add subl_config function
    organize vars
    
     Changes to be committed:
    	modified:   scripts/config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -15,16 +15,25 @@ echo "# --------------------------------------------"
 [ -z "$BACKUP" ] && BACKUP="$CONFIG/backup"
 [ -z "$SYNCED" ] && SYNCED="$HOME/Dropbox/Config"
 
-# config files
+# system and program config files
 CONF1="$HOME/.bashrc"
 CONF2="$HOME/.inputrc"
 CONF3="$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings"
 CONF4="$HOME/.config/sublime-text-3/Packages/User/Monokai - Spacegray.tmTheme"
-CONF5="$HOME/.config/sublime-text-3/Packages/User/Monokai-Spacegray.sublime-theme"
+CONF5="$HOME/.config/sublime-text-3/Packages/User/Monokai - Spacegray.sublime-theme"
 CONF6="$HOME/.muttrc"
 CONF7="$HOME/.tmux.conf"
 CONF8="$HOME/.vimrc"
 CONF9="$HOME/.gitignore_global"
+
+# config files copied from repositories
+REPO3="$CONFIG/subl/monokai-spacegray/subl.conf"
+REPO4="$CONFIG/subl/monokai-spacegray/Monokai - Spacegray.tmTheme"
+REPO5="$CONFIG/subl/monokai-spacegray/Monokai - Spacegray.sublime-theme"
+REPO6="$CONFIG/mutt/colors/mutt-colors-solarized-dark-16.muttrc"
+REPO7="$CONFIG/tmux/tmux.conf"
+REPO8="$CONFIG/vim/vim.conf"
+REPO9="$CONFIG/git/gitignore_global"
 
 # --------------------------  BACKUPS
 
@@ -51,6 +60,28 @@ do_backup() {
     debug
 
     return 0
+}
+
+# --------------------------  SUBL CONFIG
+
+# clone or pull git repo and copy repo files into proper places
+set_subl_config() {
+    local repo_url="$1"
+    local conf_files=( "$CONF3" "$CONF4" "$CONF5" )
+    local repo_files=( "$REPO3" "$REPO4" "$REPO5" )
+    local repo_dir=$(trim_shortest_right_pattern "$REPO3" "/")
+
+    # update or clone repository
+    [ -d $repo_dir ] && { cd $repo_dir; git pull; cd - >/dev/null; } || git clone "$repo_url" "$repo_dir"
+
+    # copy config files to proper locations
+    for (( i=0; i<${#repo_files[@]}; i++ ))
+    do
+        cp "${repo_files[$i]}" "${conf_files[$i]}" && success "configured: ${conf_files[$i]}"
+    done
+
+    RET="$?"
+    debug
 }
 
 # --------------------------  GIT CONFIG
@@ -131,53 +162,43 @@ pause "" true
 do_backup               "$CONF1 $CONF2 $CONF3 $CONF4 $CONF5 $CONF6 $CONF7 $CONF8 $CONF9"
 
 # aliases (to practice terminal commands for Linux certification exams, I'm not using aliases at the moment)
-#set_sourced_config      "$HOME/.bashrc" \
-#                        "https://gist.github.com/9d74e08779c1db6cb7b7" \
+#set_sourced_config      "https://gist.github.com/9d74e08779c1db6cb7b7" \
+#                        "$HOME/.bashrc" \
 #                        "$CONFIG/bash/aliases/bash_aliases" \
 #                        "\n# source alias file\nif [ -f $CONFIG/bash/aliases/bash_aliases ]; then\n   . $CONFIG/bash/aliases/bash_aliases\nfi"
 
 # mutt color scheme
-set_sourced_config      "$HOME/.muttrc" \
-                        "https://github.com/altercation/mutt-colors-solarized.git" \
-                        "$CONFIG/mutt/colors/mutt-colors-solarized-dark-16.muttrc" \
-                        "# source colorscheme file\nsource $CONFIG/mutt/colors/mutt-colors-solarized-dark-16.muttrc"
+set_sourced_config      "https://github.com/altercation/mutt-colors-solarized.git" \
+                        "$CONF6" \
+                        "$REPO6" \
+                        "# source colorscheme file\nsource $REPO6"
 
 # tmux config
-set_sourced_config      "$HOME/.tmux.conf" \
-                        "https://gist.github.com/3247d5a1c172167e593c.git" \
-                        "$CONFIG/tmux/tmux.conf" \
-                        "source-file $CONFIG/tmux/tmux.conf"
+set_sourced_config      "https://gist.github.com/3247d5a1c172167e593c.git" \
+                        "$CONF7" \
+                        "$REPO7" \
+                        "source-file $REPO7"
 
 # vim config
-set_sourced_config      "$HOME/.vimrc" \
-                        "https://gist.github.com/00a60c7355c27c692262.git" \
-                        "$CONFIG/vim/vim.conf" \
-                        "\" source config file\n:so $CONFIG/vim/vim.conf"
+set_sourced_config      "https://gist.github.com/00a60c7355c27c692262.git" \
+                        "$CONF8" \
+                        "$REPO8" \
+                        "\" source config file\n:so $REPO8"
 
 [ -d "$SYNCED/vim" ] || { mkdir -pv "$SYNCED/vim"; notify3 "note: vim spellfile will be located in $SYNCED/vim, you can change this in $CONFIG/vim/vim.conf"; }
 
 # terminal profile (can't find profile file in new Ubuntu 16.04)
-#set_copied_config       "$HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml" \
-#                        "https://gist.github.com/dad1663d2463db32c6e8.git" \
+#set_copied_config       "https://gist.github.com/dad1663d2463db32c6e8.git" \
+#                        "$HOME/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml" \
 #                        "$CONFIG/terminal/profile/gconf.xml"
 
 # sublime text
 mkdir -p "$HOME/.config/sublime-text-3/Packages/User"
-set_copied_config       "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" \
-                        "https://gist.github.com/2ff3aa9ce91ff6e0e706.git" \
-                        "$CONFIG/subl/config/subl.conf"
+set_subl_config         "https://github.com/keegoid/monokai-spacegray.git"
 
-set_copied_config       "$HOME/.config/sublime-text-3/Packages/User/Monokai - Spacegray.tmTheme" \
-                        "https://github.com/keegoid/monokai-spacegray.git" \
-                        "$CONFIG/subl/color_schemes/monokai-spacegray/Monokai - Spacegray.tmTheme"
-
-set_copied_config       "$HOME/.config/sublime-text-3/Packages/User/Monokai-Spacegray.sublime-theme" \
-                        "https://gist.github.com/1c5164d1f9e71d570eda.git" \
-                        "$CONFIG/subl/themes/Monokai-Spacegray.sublime-theme"
-
-set_copied_config       "$HOME/.gitignore_global" \
-                        "https://gist.github.com/efa547b362910ac7077c.git" \
-                        "$CONFIG/git/gitignore_global"
+set_copied_config       "https://gist.github.com/efa547b362910ac7077c.git" \
+                        "$CONF9" \
+                        "$REPO9"
 
 set_git_config          "$HOME/.gitconfig"
 
