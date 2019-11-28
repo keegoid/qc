68429d201c68218fdb34ae8b7b5f8c7742e754ef
Keegan Mullaney <keeganmullaney@gmail.com>
2019-11-28 12:49:38 -0800

    replace git-annex with syncthing
    syncthing is awesome

--- script/apts.sh
+++ script/apts.sh
@@ -108,7 +108,7 @@ qc_git_lfs() {
 
 # --------------------------  DEFAULT APT PACKAGES
 
-DEFAULT_WORKSTATION_LIST='arp-scan apt-transport-https autojump git git-annex gnupg2 links mutt net-tools pinta tree vlc x11vnc xclip'
+DEFAULT_WORKSTATION_LIST='arp-scan apt-transport-https autojump git gnupg2 links mutt net-tools pinta syncthing tree vlc x11vnc xclip'
 DEFAULT_DEV_LIST='autoconf automake build-essential byobu checkinstall dconf-cli shellcheck silversearcher-ag tidy tilix xdotool vim-gtk'
 
 # --------------------------  PROMPT FOR PROGRAMS
